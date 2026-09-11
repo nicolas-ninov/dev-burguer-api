2dd4b55b40c4fd7de47b06f9ddc1818705ee351d
nicolas-ninov <nicolasninoff2722@gmail.com>
2026-09-11 07:54:22 -0300

    feat: add product update functionality

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -4,11 +4,12 @@ import Product from '../models/Product.js';
 
 
 class ProductController {
-    async store(req, res){
+    async store(req, res) {
         const schema = Yup.object({
             name: Yup.string().required(),
             price: Yup.number().required(),
             category_id: Yup.string().required(),
+            offer: Yup.boolean(),
         });
 
         try {
@@ -19,23 +20,67 @@ class ProductController {
             });
         }
 
-        const { name, price, category_id } = req.body;
+        const { name, price, category_id, offer } = req.body;
         const { filename } = req.file
-        
+
         const newProduct = await Product.create({
             name,
             price,
             category_id,
-            path: filename
+            path: filename,
+            offer
         });
 
         return res.status(201).json(newProduct);
     }
 
+    async update(req, res) {
+        const schema = Yup.object({
+            name: Yup.string(),
+            price: Yup.number(),
+            category_id: Yup.string(),
+            offer: Yup.boolean(),
+        });
+
+        try {
+            schema.validateSync(req.body, { abortEarly: false });
+        } catch (err) {
+            return res.status(400).json({
+                error: err.errors
+            });
+        }
+
+        const { name, price, category_id, offer } = req.body;
+        const { id } = req.params
+
+        let path
+        if (req.file) {
+            const { filename } = req.file
+            path = filename
+        }
+
+
+        const updatedProduct = await Product.update({
+            name,
+            price,
+            category_id,
+            path,
+            offer
+        }, 
+        {
+            where: {
+                id
+            }
+        }
+    );
+
+        return res.status(200).json();
+    }
+
     async index(_req, res) {
         const products = await Product.findAll({
             include: {
-                model: Category, 
+                model: Category,
                 as: 'category',
                 attributes: ['id', 'name']
             },
